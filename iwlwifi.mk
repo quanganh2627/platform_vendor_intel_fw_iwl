44804cdb14e0639673294137a9de94a38dbeb953
Mihai Serban <mihai.serban@intel.com>
2014-03-26 18:49:17 +0200

    Update firmware files path
    Change-Id: I12d99b39d39a3e9531270e779a402fa5dc71cd85
    Signed-off-by: Mihai Serban <mihai.serban@intel.com>

--- iwlwifi.mk
+++ iwlwifi.mk
@@ -1,7 +1,7 @@
 # Wifi firmware
 PRODUCT_COPY_FILES += \
-        vendor/intel/hardware/PRIVATE/wlan/iwl-firmware/iwlwifi-3160-8.ucode:system/etc/firmware/iwlwifi-3160-8.ucode \
-        vendor/intel/hardware/PRIVATE/wlan/iwl-firmware/iwlwifi-7260-8.ucode:system/etc/firmware/iwlwifi-7260-8.ucode \
-        vendor/intel/hardware/PRIVATE/wlan/iwl-firmware/iwlwifi-7265-8.ucode:system/etc/firmware/iwlwifi-7265-8.ucode \
+        vendor/intel/hardware/wlan/iwl-firmware/iwlwifi-3160-8.ucode:system/etc/firmware/iwlwifi-3160-8.ucode \
+        vendor/intel/hardware/wlan/iwl-firmware/iwlwifi-7260-8.ucode:system/etc/firmware/iwlwifi-7260-8.ucode \
+        vendor/intel/hardware/wlan/iwl-firmware/iwlwifi-7265-8.ucode:system/etc/firmware/iwlwifi-7265-8.ucode \
 
 # end of file
